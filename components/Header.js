ad26a56e31b201294cad8df065191d2f525f6c2f
Jeron Wong <ThisIsJeron@users.noreply.github.com>
2024-01-15 19:32:39 -0800

    Add first visit check and open about modal

--- components/Header.js
+++ components/Header.js
@@ -14,6 +14,15 @@ const Header = () => {
   const closeAboutModal = () => {
     setAboutModalOpen(false);
   };
+
+  useEffect(() => {
+    const firstVisit = localStorage.getItem('firstVisit');
+
+    if (!firstVisit) {
+      openAboutModal();
+      localStorage.setItem('firstVisit', 'true');
+    }
+  }, []);
   /*
   useEffect(() => {
     // Check for saved theme preference in localStorage
@@ -74,16 +83,16 @@ const Header = () => {
           About
         </button>
         {aboutModalOpen && (
-          <div className="fixed z-10 inset-0 overflow-y-auto">
+          <div className="fixed z-10 inset-0 overflow-y-auto" onClick={closeAboutModal}>
             <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
               <div className="fixed inset-0 transition-opacity" aria-hidden="true">
                 <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
               </div>
               <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
               <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4 text-black">
-                  <h2>About rejected.lol</h2>
-                  <p>I was recently rejected again for a full stack software engineer role and was particularly upset. After talking with friends who were also looking for software engineering roles, we started sharing rejection letters we received and realized it would be really interesting to see the various corporate jargon that we were getting from these rejection letters. This site is dedicated for everyone who is getting rejection letters, whether it's for your job search or college search. It's easy to get demoralized after seeing so many rejection letters, but we all get rejected many times in our lives and it's important to not take these letters personally, and to dust yourself off and get back up. </p>
+                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4 text-black pt-4">
+                  <h1 className='justify-center text-2xl font-bold'>About rejected.lol</h1>
+                  <p className='pt-8'>I was recently rejected again for a full stack software engineer role and was particularly upset. After talking with friends who were also looking for software engineering roles, we started sharing rejection letters we received and realized it would be really interesting to see the various corporate jargon that we were getting from these rejection letters. This site is dedicated for everyone who is getting rejection letters, whether it's for your job search or college search. It's easy to get demoralized after seeing so many rejection letters, but we all get rejected many times in our lives and it's important to not take these letters personally, and to dust yourself off and get back up. </p>
                   <p>- @thisisjeron</p>
                 </div>
                 <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
